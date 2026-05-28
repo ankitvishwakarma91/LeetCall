72f0a43612ce63f5d04fe8d1f2bd6980137ee463
ankitvishwakarma9137 <ankitvishwakarma9137@gmail.com>
2026-05-28 14:26:24 +0530

    start

---
+++ src/main/java/com/example/leetcall/LeetCallApplication.java
@@ -0,0 +1,13 @@
+package com.example.leetcall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetCallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeetCallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/leetcall/LeetCallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.leetcall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetCallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
